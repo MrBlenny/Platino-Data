d7dfac8c8c791d9b41fb09f9e0de48f78b824c42
DaveRevay2 <daverevay@gmail.com>
2017-03-19 14:07:33 +1100

    Added publish script

---
+++ scripts/publish.js
@@ -0,0 +1,41 @@
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+const fm = require('front-matter');
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
+
+
+const parseFolder = (folderPath) => {
+    // This will parse a folder with front matter
+    const parseFile = (path) => fs.readFileAsync(path, 'utf8').then((data) => {
+        const content = fm(data);
+        content.id = getFileName(path);
+        return content;
+    });
+    const parseFiles = (files) => Promise.map(files, (fileName) => parseFile(folderPath+'/'+fileName));
+    return fs.readdirAsync(folderPath).then(parseFiles);
+};
+
+const getFileName = (fullPath) => {
+    const nameAndExt = fullPath.replace(/^.*[\\\/]/, '');
+    const name = nameAndExt.substr(0, nameAndExt.lastIndexOf('.'));
+    return name;
+};
+
+const outputFolderSummary = (path, parsedContent) => {
+    fs.writeFile(path, JSON.stringify(parsedContent));
+};
+
+// Remove old output
+rimraf('./output', () => {
+    // Create folders
+    mkdirp('./output');
+//    mkdirp('./output/projects');
+//    mkdirp('./output/news');
+    // Parse folders
+    parseFolder('./src/news').then(items => outputFolderSummary('./output/news.json', items));
+    parseFolder('./src/projects').then(items => outputFolderSummary('./output/projects.json', items));
+});
+
+
+//parseFile('./src/news/chatswood-place-1.md');
---
+++ src/news/chatswood-place-1.md
@@ -0,0 +1,14 @@
+---
+title: News Article 1
+author: Audrey
+project: Chatswood Place
+---
+
+## Some markdown
+
+This is some text about some stuff that happened sometime ago
+
+* bullets
+* more bullets
+
+And some more text for good measure.
